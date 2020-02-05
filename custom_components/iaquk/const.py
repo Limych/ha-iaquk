0ec3aa9665a67f19503efb2d6ac2904b66dd9ef1
Andrey Khrolenok <andrey@khrolenok.ru>
2020-02-05 16:25:01 +0300

    Version bump

--- custom_components/iaquk/const.py
+++ custom_components/iaquk/const.py
@@ -2,7 +2,7 @@
 
 # Base component constants
 DOMAIN = "iaquk"
-VERSION = "1.1.2"
+VERSION = "1.2.0"
 ISSUE_URL = "https://github.com/Limych/ha-iaquk/issues"
 ATTRIBUTION = None
 DATA_IAQUK = 'iaquk'
