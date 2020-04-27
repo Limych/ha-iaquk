b1b3459a999b7950e9347d384909c98fdb63d846
Andrey Khrolenok <andrey@khrolenok.ru>
2020-04-28 02:12:18 +0300

    Fix dev-setup.sh and add Idea config

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+pip3 install pre-commit
 pip3 install -r requirements.txt -r requirements-dev.txt --user
 pre-commit install
 pre-commit autoupdate
 chmod a+x update_tracker.py
 
-if [ -d /run/user/1001/gvfs/smb-share:server=hassio,share=config/ ]; then
-    ln -s /run/user/1001/gvfs/smb-share:server=hassio,share=config/ dev-config
+CONFIG=/run/user/$(id -u)/gvfs/smb-share:server=hassio,share=config/
+if [ -d "$CONFIG" ] && [ ! -e dev-config ]; then
+    ln -s "$CONFIG" dev-config
 fi
